1e8be3318a9683431bdeb952fc0d463391959e32
Joern Engel <joern@logfs.org>
2007-10-26 03:05:41 +0200

    Define s32 and friends.

--- kerncompat.h
+++ kerncompat.h
@@ -46,17 +46,26 @@
 #endif
 
 #ifdef __CHECKER__
+typedef unsigned char u8;
+typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned int __u32;
 typedef unsigned long long u64;
-typedef unsigned char u8;
-typedef unsigned short u16;
+typedef char s8;
+typedef short s16;
+typedef int s32;
+typedef int __s32;
+typedef long long s64;
 #else
 #include <asm/types.h>
+typedef __u8 u8;
+typedef __u16 u16;
 typedef __u32 u32;
 typedef __u64 u64;
-typedef __u16 u16;
-typedef __u8 u8;
+typedef __s8 s8;
+typedef __s16 s16;
+typedef __s32 s32;
+typedef __s64 s64;
 #endif
 
 
